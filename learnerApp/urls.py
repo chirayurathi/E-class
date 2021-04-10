77ebb072a9085da1cc36938ff25b124d2370bedf
chirayurathi <chirayurathi4@gmail.com>
2021-04-10 21:08:26 +0530

    login frontend

--- learnerApp/urls.py
+++ learnerApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('',views.home),
+    path('',views.home, name="home"),
+    path('login/',views.login, name="login")
 ]
--- learnerApp/views.py
+++ learnerApp/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'learnerApp/home.html')
+def login(request):
+    return render(request,'learnerApp/login.html')
